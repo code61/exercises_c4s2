418322822d08d7b61b0a2933b23bfc0b291041b5
George Knee <georgeknee@googlemail.com>
2014-06-02 12:22:07 +0100

    finished Person class

--- person.rb
+++ person.rb
@@ -11,5 +11,26 @@
 #
 class Person
 
-  
+	def initialize(params)
+		@name = params[:name]
+		@dob = params[:dob]
+	end
+
+	def name
+		@name
+	end
+
+	def dob
+		@dob
+	end
+
+	def first_name
+		@name.split.first
+	end
+
+	def last_name
+		# @name.spit.last
+		@name.split[1..-1].join(' ')
+	end
+
 end
